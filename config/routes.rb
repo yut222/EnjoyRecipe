e23e2442eb7cd8795f68c191011ec59075d1e6c8
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-03-16 12:44:42 +0000

    [Add] ログイン画面確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+
   root 'homes#index'
 
   get 'recipes/tweet', to: 'homes#tweet_index'
+
+  # URL /users/sign_in ...
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
