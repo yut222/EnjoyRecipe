c908d83542271b541fd5ac5fe5d10c55988fd7b0
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-03-21 10:11:33 +0000

    [Add] お気に入り作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,19 @@
 class FavoritesController < ApplicationController
 
+  before_action :recipe_params
 
+  def create
+    current_user.favorites.create(recipe_id: @recipe.id)
+  end
+
+  def destroy
+    current_user.favorites.find_by(recipe_id: @recipe.id).destroy
+  end
+
+    private
+
+  def recipe_params
+    @recipe = Recipe.find(params[:recipe_id])
+  end
 
 end
