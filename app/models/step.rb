035ed06107941c73e371b4fedb87996d2ca97248
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-03-22 09:44:04 +0000

    [Add] アソシエーション作成

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,9 @@
 class Step < ApplicationRecord
+
+  belongs_to :recipe
+
+  validates :direction, presence: true
+
+  #画像アップロード carrierwave
+  mount_uploader :image, StepImageUploader
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,8 @@
 class Tag < ApplicationRecord
+
+  has_many :recipe_tag_relations, dependent: :delete_all
+  has_many :recipes, through: :recipe_tag_relations
+
+  validates :name, presence: true, uniqueness: { case_sensitive:  true }
+
 end
