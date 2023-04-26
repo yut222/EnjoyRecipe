554068de4e494209ce2fb1f98e5362f3a36ef22a
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-04-26 10:44:44 +0000

    [Fix] seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,18 @@
 
 
 # タグ検索
-require './db/seeds/tag.rb'
+# require './db/seeds/tag.rb'
 
-Dir[File.expand_path('./db/seeds' << '/*.rb')].each do |file|
-  require file
-end
+# Dir[File.expand_path('./db/seeds' << '/*.rb')].each do |file|
+#   require file
+# end
+
+Tag.first_or_create([
+  { name: '栄養満点' },
+  { name: '簡単・時短' },
+  { name: 'おもてなし' },
+  { name: 'おつまみ' },
+  { name: '作り置き' },
+  { name: '和食' },
+  { name: 'スイーツ' }
+])
--- db/seeds/tag.rb
+++
@@ -1,9 +0,0 @@
-Tag.first_or_create([
-  { name: '栄養満点' },
-  { name: '簡単・時短' },
-  { name: 'おもてなし' },
-  { name: 'おつまみ' },
-  { name: '作り置き' },
-  { name: '和食' },
-  { name: 'スイーツ' }
-])
