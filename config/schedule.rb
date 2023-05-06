c928054a6cbd726804d6e83c8814cdea3ad7d223
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-05-06 10:11:23 +0000

    add 通知機能

--- config/schedule.rb
+++ config/schedule.rb
@@ -26,7 +26,7 @@ require File.expand_path(File.dirname(__FILE__) + '/environment')
 rails_env = ENV['RAILS_ENV'] || :development
 
 # cronを実行する環境変数をセット
-set :environment, :production
+set :environment, rails_env
 
 # cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
